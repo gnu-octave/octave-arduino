bf8d1daa1514d726c6148e69099c9890d64b6cff
John Donoghue <john.donoghue@ieee.org>
2020-02-03 20:14:18 -0500

    maint: bump version to 0.5.0
    * DESCRIPTION, NEWS: set version number
    
    * inst/+arduinoio/lib/LibraryBase.h: set version, update (c)
    
    * doc/arduino.texi: update version and (c)

--- inst/+arduinoio/lib/LibraryBase.h
+++ inst/+arduinoio/lib/LibraryBase.h
@@ -1,6 +1,6 @@
 /*
  * Octave arduino library interface
- * Copyright (C) 2018 John Donoghue <john.donoghue@ieee.org>
+ * Copyright (C) 2018-2020 John Donoghue <john.donoghue@ieee.org>
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
 #include <Arduino.h>
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 4
+#define VERSION_MINOR 5
 #define VERSION_PATCH 0
 
 // sparkfun samed21 dev/mini
