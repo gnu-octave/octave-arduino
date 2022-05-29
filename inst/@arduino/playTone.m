23383b128019a93d9dbec95585f2356c86d11dda
John Donoghue <john.donoghue@ieee.org>
2022-05-29 17:41:08 -0400

    * inst/@arduino/playTone.m: bit shift freqh and durationh for correct value (Bug #62538)

--- inst/@arduino/playTone.m
+++ inst/@arduino/playTone.m
@@ -61,11 +61,11 @@ function playTone (obj, pin, freq, duration)
   endif
 
   freq = uint16(freq);
-  freqh = bitand(freq/256, hex2dec('FF'));
+  freqh = bitshift (freq, -8)
   freql = bitand(freq, hex2dec('FF'));
 
   duration = uint16(duration*10);
-  durh = bitand(duration/256, hex2dec('FF'));
+  durh = bitshift (duration, -8)
   durl = bitand(duration, hex2dec('FF'));
 
   datain = uint8([pininfo.id freqh freql durh durl]);
